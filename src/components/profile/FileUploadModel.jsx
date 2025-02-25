b499c10b60d610f743e9682a73c93950890962e6
Janith Ranasinghe <janithraditha@gmail.com>
2025-02-25 23:43:45 +0530

    profile image hanle processs done

---
+++ src/components/profile/FileUploadModel.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+const FileUploadModal = ({ onClose, onFileUpload, hasImg, onDelete }) => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedFile(file);
+    };
+
+    const handleUpload = () => {
+        if (selectedFile) {
+            onFileUpload(selectedFile);
+            setSelectedFile(null);
+            onClose();
+        } else {
+            alert('Please select a file to upload.');
+        }
+    };
+
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="bg-primary p-6 rounded-lg shadow-lg w-96">
+                <h2 className="text-xl font-semibold text-white mb-4">Change Profile Picture</h2>
+                <input
+                    type="file"
+                    onChange={handleFileChange}
+                    className="block w-full text-sm text-white file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:bg-light file:text-primary hover:file:bg-accent hover:file:text-white"
+                />
+                {selectedFile && (
+                    <p className="text-white mt-2 text-sm">Selected File: {selectedFile.name}</p>
+                )}
+                <div className="flex justify-end mt-4">
+                    <button
+                        type="button"
+                        onClick={onClose}
+                        className="px-4 py-2 bg-secondary text-white rounded hover:bg-opacity-90 mr-2 transition-colors"
+                    >
+                        Cancel
+                    </button>
+                    {hasImg && !selectedFile && (
+                        <button
+                            type="button"
+                            onClick={onDelete}
+                            className="flex items-center gap-2 px-4 py-2 bg-red-400 text-white rounded-md hover:bg-red-500 mr-2 transition-colors"
+                        >
+                            Delete
+                        </button>
+                    )}
+                    <button
+                        type="button"
+                        onClick={handleUpload}
+                        className="px-4 py-2 bg-accent text-white rounded hover:bg-opacity-90 transition-colors"
+                    >
+                        {hasImg ? 'Update' : 'Upload'}
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FileUploadModal;
--- src/components/profile/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo.jsx
@@ -1,17 +1,90 @@
-import { Mail, Phone, MapPin } from 'lucide-react';
+import {Mail, MapPin, Phone} from 'lucide-react';
+import Cookies from 'js-cookie';
+import axiosFetch from "../utils/Auth.js";
+import {useEffect, useState} from "react";
+import FileUploadModal from "./FileUploadModel.jsx";
 
-export default function ProfileInfo({ profile, isEditing, editedProfile, onChange }) {
+export default function ProfileInfo({profile, isEditing, editedProfile, onChange}) {
+    const userId = Cookies.get("userId");
+    const [user, setUser] = useState({});
+    const [imageUrl, setImageUrl] = useState('');
+    const [imageId, setImageId] = useState('');
+    const [isFileEditing, setIsFileEditing] = useState(false);
+
+
+    const getUserData = async () => {
+        return axiosFetch.get(`api/v1/customer/${userId}`);
+    }
+    useEffect(() => {
+        getUserData().then(r => setUser(r?.data?.object))
+    }, [userId]);
+
+    useEffect(() => {
+        setImageUrl(user?.profileImage?.resourceUrl)
+        setImageId(user?.profileImage?.propertyId)
+    }, [user?.profileImage?.propertyId]);
+
+    const handleImageUpload = async (file) => {
+        const formData = new FormData();
+        formData.append("profileImage", file);
+        console.log(formData)
+
+        try {
+            if (imageUrl) {
+                await axiosFetch.put(`api/v1/profile-images/${imageId}`, formData);
+                alert("Image successfully updated.");
+            } else {
+                const response = await axiosFetch.post(`api/v1/profile-images/${userId}`, formData);
+                console.log(response)
+                alert("Image successfully uploaded.");
+            }
+            window.location.reload();
+        } catch (error) {
+            alert("Failed to upload image");
+        }
+    };
+
+    const deleteProfilePic = async () => {
+        const isConfirm = window.confirm("Are you sure you want to delete your profile picture?");
+
+        if (isConfirm) {
+            try {
+                await axiosFetch.delete(`api/v1/profile-images/${imageId}`);
+                window.location.reload();
+            } catch (error) {
+                alert("Something went wrong.");
+            }
+        }
+    };
+
     return (
         <div className="p-8">
             <div className="flex items-center gap-8 mb-8">
-                <img
-                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&h=400&fit=crop"
-                    alt="Profile"
-                    className="w-32 h-32 rounded-full object-cover border-4 border-accent"
-                />
+                <div
+                    className={`w-32 h-32 shadow-md rounded-full flex items-center justify-center 
+                            hover:cursor-pointer ${imageUrl ?
+                        "bg-no-repeat bg-cover bg-center" : "bg-[#D9D9D9] text-gray-800"}`}
+                    style={imageUrl ? {backgroundImage: `url("${imageUrl}")`} : {}}
+                    onClick={() => setIsFileEditing(true)}
+                >
+                    {!imageUrl && (
+                        <span className="text-4xl font-bold">
+                                    {user.userName ? user.userName.split(" ").map((word) =>
+                                        word[0]).join("") : null}
+                                </span>
+                    )}
+                </div>
+                {isFileEditing && (
+                    <FileUploadModal
+                        onClose={() => setIsFileEditing(false)}
+                        onFileUpload={handleImageUpload}
+                        hasImg={imageUrl}
+                        onDelete={deleteProfilePic}
+                    />
+                )}
                 <div className="flex-1">
                     {!isEditing ? (
-                        <h2 className="text-3xl font-bold text-secondary">{profile.name}</h2>
+                        <h2 className="text-3xl font-bold text-secondary">{user.userName}</h2>
                     ) : (
                         <input
                             type="text"
@@ -22,7 +95,7 @@ export default function ProfileInfo({ profile, isEditing, editedProfile, onChang
                         />
                     )}
                     {!isEditing ? (
-                        <p className="text-accent mt-2">{profile.occupation}</p>
+                        <p className="text-accent mt-2">{user.gender}</p>
                     ) : (
                         <input
                             type="text"
@@ -54,9 +127,9 @@ export default function ProfileInfo({ profile, isEditing, editedProfile, onChang
                     <h3 className="text-xl font-semibold text-secondary">Contact Information</h3>
                     <div className="grid gap-4">
                         <div className="flex items-center gap-3">
-                            <Mail className="text-accent" />
+                            <Mail className="text-accent"/>
                             {!isEditing ? (
-                                <span>{profile.email}</span>
+                                <span>{user.email}</span>
                             ) : (
                                 <input
                                     type="email"
@@ -68,9 +141,9 @@ export default function ProfileInfo({ profile, isEditing, editedProfile, onChang
                             )}
                         </div>
                         <div className="flex items-center gap-3">
-                            <Phone className="text-accent" />
+                            <Phone className="text-accent"/>
                             {!isEditing ? (
-                                <span>{profile.phone}</span>
+                                <span>{user.phone}</span>
                             ) : (
                                 <input
                                     type="tel"
@@ -82,9 +155,23 @@ export default function ProfileInfo({ profile, isEditing, editedProfile, onChang
                             )}
                         </div>
                         <div className="flex items-center gap-3">
-                            <MapPin className="text-accent" />
+                            <MapPin className="text-accent"/>
                             {!isEditing ? (
-                                <span>{profile.location}</span>
+                                <span>{user.address}</span>
+                            ) : (
+                                <input
+                                    type="text"
+                                    name="location"
+                                    value={editedProfile.location}
+                                    onChange={onChange}
+                                    className="bg-light rounded px-3 py-1 flex-1"
+                                />
+                            )}
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <MapPin className="text-accent"/>
+                            {!isEditing ? (
+                                <span>{user.postalCode}</span>
                             ) : (
                                 <input
                                     type="text"
